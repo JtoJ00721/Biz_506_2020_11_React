f201362da197f6c6e2659db270cc82c49c298215
JtoJ00721 <moonlf2000@naver.com>
2020-11-03 09:51:11 +0900

    react 파사드 패턴

--- react-phone-book/src/phone-book/PhoneInsert.jsx
+++ react-phone-book/src/phone-book/PhoneInsert.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-const PhoneInsert = () => {
+// onClickSave함수의 실제 사용은
+// Insert 컴포넌트에서 실행하기
+const PhoneInsert = ({ onClickSave }) => {
   return (
     <form>
       <input placeholder="이름" />
       <input placeholder="전화번호" />
-      <button type="button">저장</button>
+      <button onClick={onClickSave} type="button">
+        저장
+      </button>
     </form>
   );
 };
