9efe43d86333890bef16b32b6970f6576fce99a4
JtoJ00721 <moonlf2000@naver.com>
2020-11-11 12:23:27 +0900

    class. 방식과 함수 방식의 props 사용방법 차이

--- todo-list/src/component/TodoMain.jsx
+++ todo-list/src/component/TodoMain.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../css/TodoMain.css";
 import TodoInsert from "./TodoInsert";
+import TodoList from "./TodoList";
 
 /**
  * class 방식의 Component
@@ -9,12 +10,23 @@ import TodoInsert from "./TodoInsert";
  * 함수방식에서 바로 나타나던 return문이 render() method 내부에 위치한다.
  */
 class TodoMain extends Component {
+  //class 방식에서 state 변수 선언하기
+  state = {
+    id: 3,
+    todoList: [
+      { id: 0, text: "빼빼로데이지롱", isComplete: false },
+      { id: 1, text: "빙빙이 왜 안줘! ><", isComplete: false },
+      { id: 2, text: "읭?", isComplete: false },
+      { id: 3, text: "마누라한테 빼빼로도 안줘? ><", isComplete: true },
+    ],
+  };
+
   render() {
     return (
       <main className="todo-main">
         <h3>TO-DO List</h3>
         <TodoInsert />
-        <p>TODO LIST</p>
+        <TodoList todoList={this.state.todoList} />
       </main>
     );
   }
