1057d5de5ebe45a04ad24d20fdc4f49706c8c120
JtoJ00721 <moonlf2000@naver.com>
2020-11-11 16:42:47 +0900

    todoList

--- todo-list/src/component/TodoItem.jsx
+++ todo-list/src/component/TodoItem.jsx
@@ -3,17 +3,37 @@ import "../css/TodoItem.css";
 
 class TodoItem extends Component {
   render() {
-    const { todo } = this.props;
+    const { todo, onToggle, onDeleteItem } = this.props;
+    const onClick = (e, id) => {
+      if (e.target.className === "delete-item") {
+        if (window.confirm("삭제? ><")) {
+          // alert("없어졌지롱");
+          onDeleteItem(id);
+          return false;
+        }
+      } else {
+        onToggle(id);
+      }
+    };
 
+    // 매개변수 전달이 필요없는 이벤트 핸들러
+    // onClick={() => onToggle(id)} : 매개변수 전달이 필요한 event 핸들러
     return (
-      <div className="todo-item">
+      <div className="todo-item" onClick={(e) => onClick(e, todo.id)}>
         <div className="delete-item">&times;</div>
         <div className={`todo-text ${todo.isComplete ? "checked" : ""}`}>
           {todo.text}
         </div>
-        {todo.isComplete && <div>&#x2713;</div>}
+        {todo.isComplete && <div className="check-mark">&#x2713;</div>}
       </div>
     );
+    /**
+     * {todo.isComplete && <div><div/>}
+     * {}로 묶음 부분은 내부의 연산 결과에 따라 rendering을 수행하는 코드
+     * todo.iscomplete값이 true 인 경우만
+     * <div><div/>를 rendering하여 화면에 보이라는
+     * 조건부 rendering
+     */
   }
 }
 
--- todo-list/src/component/TodoMain.jsx
+++ todo-list/src/component/TodoMain.jsx
@@ -10,23 +10,67 @@ import TodoList from "./TodoList";
  * 함수방식에서 바로 나타나던 return문이 render() method 내부에 위치한다.
  */
 class TodoMain extends Component {
+  id = 3;
   //class 방식에서 state 변수 선언하기
   state = {
-    id: 3,
     todoList: [
       { id: 0, text: "빼빼로데이지롱", isComplete: false },
       { id: 1, text: "빙빙이 왜 안줘! ><", isComplete: false },
       { id: 2, text: "읭?", isComplete: false },
-      { id: 3, text: "마누라한테 빼빼로도 안줘? ><", isComplete: true },
+      { id: 3, text: "마누라한테 빼빼로도 안줘!! ><", isComplete: true },
     ],
+  };
+
+  /**
+   * 클래스 방식의 main에서 이벤트 함수를 선언하는 방법
+   * const 키워드 없이 함수를 선언하라
+   */
+  onToggle = (id) => {
+    // id값을 기준으로 todoList 변수의 idComplete를 변경
+    // 1. this.state에서 todoList 변수를 추출
+    const { todoList } = this.state;
+    const compTodoList = todoList.map((todo) => {
+      if (todo.id === Number(id))
+        return { ...todo, isComplete: !todo.isComplete };
+      else return todo;
+    });
+    /**
+     * 클래스 방식 Component에서는 state 변수를 setting 하기위한
+     * setter를 별도로 만들지 않는다.
+     *
+     * state변수를 setting하기 위해서는
+     * this.setState()라는 공통함수를 사용한다
+     *
+     * this.setState( {state변수 : 새로운 값} ) 형식으로 setting을 한다.
+     */
+    this.setState({ todoList: compTodoList });
+  };
+
+  onCreate = (todo) => {
+    const newTodoList = [
+      ...this.state.todoList,
+      { id: this.id++, text: todo, isComplete: false },
+    ];
+    this.setState({ todoList: newTodoList });
+  };
+
+  onDeleteItem = (id) => {
+    const deleteTodoList = this.state.todoList.filter((todo) => {
+      if (todo.id !== Number(id)) return todo;
+    });
+    this.setState({ todoList: deleteTodoList });
   };
 
   render() {
     return (
       <main className="todo-main">
         <h3>TO-DO List</h3>
-        <TodoInsert />
-        <TodoList todoList={this.state.todoList} />
+        <TodoInsert onCreate={this.onCreate} />
+        <TodoList
+          todoList={this.state.todoList}
+          onToggle={this.onToggle}
+          onDeleteItem={this.onDeleteItem}
+        />
       </main>
     );
   }
