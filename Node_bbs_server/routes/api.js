3aaafeae961a499966809710e7bcc91fbe6e4fb2
JtoJ00721 <moonlf2000@naver.com>
2020-11-13 16:47:33 +0900

    MYSQL 연결

--- Node_bbs_server/routes/api.js
+++ Node_bbs_server/routes/api.js
@@ -4,6 +4,7 @@
 // 현재 nodejs가 지원하는 문법
 const express = require("express");
 const router = express.Router();
+const { bbsDao } = require("../models/index");
 
 router.get("/", (req, res) => {
   // res.send("반갑지롱");
@@ -16,7 +17,28 @@ router.get("/bbsList", (req, res) => {
     { id: 0, writer: "빙빙이", subject: "게시판" },
     { id: 0, writer: "희", subject: "게시판" },
   ];
-  res.json(list);
+
+  bbsDao.findAll({ order: ["b_date_time"] }).then((bbsList) => {
+    res.json(bbsList);
+  });
+  // res.json(list);
+});
+
+router.get("/insert", (req, res) => {
+  bbsDao
+    .create({
+      b_writer: req.query.writer || "빙빙이",
+      b_date_time: Date().toString(),
+      b_subject: "농농이?",
+      b_content: "에엫따..!",
+    })
+    .then((result) => {
+      // res.json(result);
+      res.redirect("/api/bbsList");
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
